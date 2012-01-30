13b8422e044738809c6689cb2c47dfb32ae215f2
WissKI <webmaster@wiss-ki.eu>
2012-01-30 11:44:15 +0100

    first commit

---
+++ page.tpl.php
@@ -0,0 +1,94 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN" "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en">
+<head>
+   <title><?php print $head_title ?></title>
+   <?php print $head; ?>
+   <?php print $styles; ?>
+   <?php print $scripts; ?>
+</head>
+<body class="<?php print $body_classes; ?>">
+<!-- CORPUS -->
+<div id="wki-bodywrap">
+    <!-- HEADER -->
+    <div id="wki-header">
+        <div id="wki-nav">
+            <div id="wki-nav-primary"><?php print theme('links', $primary_links); ?></div>
+            <div id="wki-nav-secondary"><?php print theme('links', $secondary_links); ?></div>
+        </div>
+        <div id="wki-logo"><a href="<?php print $front_page; ?>"><img src="<?php print $logo; ?>" alt="<?php print t('Home'); ?>" id="wki-logo-image" /></a></div>
+        <div id="wki-sitename"><?php print $site_name; ?></div>
+        <div id="wki-siteslogan"><?php print $site_slogan; ?></div>
+    </div>
+    <!-- /HEADER -->
+    <!-- BORDERS -->
+    <div id="wki-border-left"></div>
+    <div id="wki-border-right"></div>
+    <!-- /BORDERS -->
+    <!-- CORPUS -->
+    <div id="wki-corpus">
+        <!-- TOPBAR -->
+        <div id="wki-topbar">
+            <?php if ($title || $maltedtitle): ?>
+            <div id="wki-title">
+                <?php if (!empty($maltedtitle)) {print $maltedtitle;} else {print $title;} ?>
+            </div>
+            <?php endif; ?>
+            <?php if ($tabs): ?>
+                <div id="wki-tabs"><?php print $tabs; ?></div>
+            <?php endif; ?>
+            <?php if ($breadcrumb): ?>
+                <!--<div id="wki-breadcrumb"><?php print $breadcrumb; ?></div>-->
+            <?php endif; ?>
+            <?php if ($mission): ?>
+                <div id="wki-mission"><?php print $mission; ?></div>
+            <?php endif; ?>
+            <?php if ($help): ?>
+                <!--<div id="wki-help"><?php print $help; ?></div>-->
+            <?php endif; ?>
+            <?php if ($messages): ?>
+                <div id="wki-messages"><?php print $messages; ?></div>
+            <?php endif; ?>
+            <?php print $top ?>
+        </div>
+        <!-- /TOPBAR -->
+        <!-- RIGHTBLOCK -->
+        <div id="wki-block-right">
+            <?php print $right; ?>
+        </div>
+        <!-- /RIGHTBLOCK -->
+        <!-- LEFTBLOCK -->
+        <div id="wki-block-left">
+        <?php if ( $pictures ): ?>
+            <div id="wki-nodeimages">
+                <div class="block-inner">
+                    <h2 class="title">Images</h2>
+                    <?php print $pictures ?>
+                </div>
+            </div>
+        <?php endif; ?>
+            <?php print $left; ?>
+        </div>
+        <!-- /LEFTBLOCK -->
+        <!-- CONTENT -->
+	<div id="wki-content">
+	    <?php print $content; ?>
+        </div>
+        <!-- /CONTENT -->
+    </div>
+    <!-- /CORPUS -->
+    <!-- FOOTER -->
+    <div id="wki-footer">
+        <div id="wki-footer-box">
+            <?php print $footerbox; ?>
+        </div>
+        <div id="wki-footer-text">
+            <?php print $footer_message; ?>
+            <?php print $footer; ?>
+        </div>
+    </div>
+    <!-- /FOOTER -->
+</div>
+<!-- /BODYWRAP -->
+<?php print $closure; ?>
+</body>
+</html>
---
+++ template.php
@@ -0,0 +1,154 @@
+<?php
+/**
+ * Override or insert variables into the page templates.
+ *
+ * @param $vars
+ *   An array of variables to pass to the theme template.
+ * @param $hook
+ *   The name of the template being rendered ("page" in this case.)
+ */
+
+function blendedmalts_preprocess_page(&$vars, $hook) {
+    $currenttype = $vars['node']->type;
+    if($currenttype=="individual") {
+        blendedmalts_viewadjust($vars, "page");
+        blendedmalts_removetab('/edit', $vars);
+        blendedmalts_changetab('/tedit', $vars);
+    }
+}
+ 
+/**
+ * Adding additional preprocess functions inside existing preprocess functions
+ * http://drupal.org/node/337022
+ */
+ function blendedmalts_preprocess_node(&$vars, $hook) {
+  $function = 'blendedmalts_preprocess_node'.'_'. $vars['node']->type;
+  if (function_exists($function)) {
+   $function($vars, $hook);
+  }
+}
+
+/**
+ * Adding additional variables for node-individual
+ */
+function blendedmalts_preprocess_node_individual(&$vars, $hook) {
+    blendedmalts_viewadjust($vars, "node");   
+    blendedmalts_removetab('/edit', $vars);
+}
+
+function blendedmalts_viewadjust(&$vars, $mode = "node") {
+  // Get the node object
+  $node = &$vars['node'];
+
+  include_once('sites/all/modules/wisski_pathbuilder/wisski_pathbuilder.inc');
+  
+  $groupid = wisski_pathbuilder_getGroupIDForIndividual(wisski_store_getObj()->wisski_ARCAdapter_delNamespace($node->title));
+
+  if(!$groupid || !isset($groupid))
+    return;
+  
+  if($mode == "page") {
+    $vars['maltedtitle'] = wisski_pathbuilder_generateGroupName($node->title, $groupid);
+  } else if($mode == "node") {
+    $block2 = module_invoke('wisski_pathbuilder', 'block', 'view', 0);
+    $block1 = module_invoke('wisski_pathbuilder', 'block', 'view', 1);
+    $vars['maltedcontent'] = '<div id="wki-content-right">' . $block2['content'] . '</div>' . '<div id="wki-content-left">' . $block1['content'] .  '</div>';
+  }              
+}
+
+function blendedmalts_removetab($label, &$vars) {
+  $tabs = explode("\n", $vars['tabs']);
+  $vars['tabs'] = '';
+    
+  foreach ($tabs as $tab) {
+    if (strpos($tab, '' . $label . '">') === FALSE) {
+      $vars['tabs'] .= $tab . "\n";
+      
+    }
+  }
+}
+
+function blendedmalts_changetab($label, &$vars) {
+  $tabs = explode("\n", $vars['tabs']);
+  $vars['tabs'] = '';
+    
+  foreach ($tabs as $tab) {
+    if (strpos($tab, '' . $label . '">') === FALSE) {
+      $vars['tabs'] .= $tab . "\n";
+    } else {
+      $text = $tab;
+      $text = str_replace('Edit', 'Edit Text', $text);
+      $node = $vars['node'];
+      $indiv = $node->title;
+      $obj = wisski_store_getObj();
+      $namespaces = $obj->wisski_ARCAdapter_getNamespaces();
+      $q = "";
+      
+      foreach ($namespaces as $name => $val) {
+        $q .= "PREFIX $name:\t<$val>\n";
+      }  
+      
+      $pred = "ecrm:P129i_is_subject_of";
+  
+      $q .= "SELECT * WHERE { <" . wisski_store_getObj()->wisski_ARCAdapter_delNamespace($indiv) . "> <" 
+        .  wisski_store_getObj()->wisski_ARCAdapter_delNamespace($pred) . "> ?x . }";
+      $rows =  wisski_store_getObj()->wisski_ARCAdapter_getStore()->query($q, 'rows');
+
+      // by Martin: if noo text  found, search via Document group path
+      if (!$rows && module_exists('wisski_textproc')) {
+        $text_uris = wisski_textproc_get_texts(wisski_store_getObj()->wisski_ARCAdapter_delNamespace($indiv));
+        $rows = array(array('x' => $text_uris[0]));
+      }
+      // end by Martin
+
+      $url = parse_url($rows[0]['x']);
+
+      if($rows[0]['x']) {
+        $vars['tabs'] .= '<li><a href="' . url() . drupal_lookup_path('source', ('content/' . wisski_store_getObj()->wisski_ARCAdapter_addNamespace($rows[0]['x'])))  . '/annotext?uri=' . urlencode(wisski_store_getObj()->wisski_ARCAdapter_delnamespace($indiv)) . '">Edit Text</a></li>';
+      } else {
+  
+        $q = "";
+        foreach ($namespaces as $name => $val) {
+          $q .= "PREFIX $name:\t<$val>\n";
+        }
+    
+        $q .= "SELECT * WHERE { <" . wisski_store_getObj()->wisski_ARCAdapter_delNamespace($indiv) . "> "
+          . "rdf:type ?x . }";
+      
+        $rows =  wisski_store_getObj()->wisski_ARCAdapter_getStore()->query($q, 'rows');
+        include_once('sites/all/modules/wisski_pathbuilder/wisski_pathbuilder.inc');
+
+        $groups = wisski_pathbuilder_getGroups();
+
+        foreach($groups as $group) {
+          $samepart = _wisski_pathbuilder_calculate_group_samepart($group);
+          if($samepart["x" . (floor(count($samepart)/2))] == $rows[0]['x']) {
+            $vars['tabs'] .= '<li><a href="' . url() . 'node/add/individual' . '/annotext/' . $group . '?uri=' . urlencode($indiv) .'">Create Text</a></li>';
+            break;
+          }
+        }
+      }
+    
+      $vars['tabs'] .= str_replace('Edit', 'Edit Form', $tab) . "\n";
+    }
+  }
+}
+
+function blendedmalts_button($element) {
+  if (isset($element['#attributes']['class'])) {
+    $element['#attributes']['class'] = 'form-'. $element['#button_type'] .' '. $element['#attributes']['class'];
+  } else {
+    $element['#attributes']['class'] = 'form-'. $element['#button_type'];
+  }
+  
+  $return_string = '<input ';
+  if ($element['#button_type'] == 'image') {
+    $return_string .= 'type="image" ';
+  } else {
+    $return_string .= 'type="submit" ';
+  }
+  $return_string .= (empty($element['#name']) ? '' : 'name="'. $element['#name'] .'" ') .'value="'. check_plain($element['#value']) .'" '. drupal_attributes($element['#attributes']) ." />\n";;
+  return $return_string;
+}
+
+?>
