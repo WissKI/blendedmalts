91cc362d2552e9dbe5d79a219398097da252984d
root <root@wisski.gnm.de>
2013-06-13 17:09:42 +0200

    Only zoom on images

---
+++ init.js
@@ -0,0 +1,19 @@
+$(document).ready(function(){
+	// Slider
+	/*
+	$('div#block-wisski_pathbuilder-2 ul').addClass('bjqs');
+	$('div#block-wisski_pathbuilder-2 div.content').bjqs({
+		'height': 180,		
+		'width': 180
+	});
+	*/
+	// Colorbox
+	$('div#block-wisski_pathbuilder-2 a.wisski-image').attr('rel', 'group1');
+	$('div#block-wisski_pathbuilder-2 a.wisski-image').colorbox({
+		rel:"group1",
+		transition:"elastic",
+		width:"75%",
+		height:"75%"
+	});
+});
+
