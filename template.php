9556c610c4cbce006b6a6fc733706a2308a63193
WissKI <m.fichtner@wiss-ki.eu>
2013-07-08 15:26:38 +0200

    Commit of most recent changes.

--- template.php
+++ template.php
@@ -205,4 +205,22 @@ function blendedmalts_node_submitted($node) {
   }
 }
 
+function blendedmalts_fieldset($element) {
+
+  if (!empty($element['#collapsible'])) {
+    drupal_add_js('misc/collapse.js');
+      
+  if (!isset($element['#attributes']['class'])) {
+    $element['#attributes']['class'] = '';
+  }
+                    
+  $element['#attributes']['class'] .= ' collapsible';
+    if (!empty($element['#collapsed'])) {
+      $element['#attributes']['class'] .= ' collapsed';
+    }
+  }
+                                        
+  return '<fieldset' . drupal_attributes($element['#attributes']) . '>' . ($element['#title'] ? '<legend>' . $element['#title'] . '</legend>' : '') . (isset($element['#description']) && $element['#description'] ? '<div class="description">' . $element['#description'] . '</div>' : '') . (!empty($element['#children']) ? $element['#children'] : '') . (isset($element['#value']) ? $element['#value'] : '') . "</fieldset>\n";
+}
+
 ?>
